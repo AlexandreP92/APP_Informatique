98d5e5ca6fbbc4eed6d4bb1a84a6536b5cfcd7b3
PAYEN <apayen@juniorisep.com>
2020-04-26 21:57:56 +0200

    Début Page d'Accueil

---
+++ P_Accueil_OFF.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link type="text/css" rel="stylesheet" href="P_Accueil_OFF.css?t=<? echo time(); ?>" media="all">
+    <meta charset="UTF-8">
+    <title>WINK FOR INFINITE MEASURES</title>
+</head>
+<body>
+    <!-- On ajoute le Header + Menu -->
+    <?php require('Header.html');
+    ?>
+    <?php require('Menu.html');
+    ?>
+
+    <!-- Contenu de la Page d'Accueil -->
+    <section>
+        <!-- Milieu -->
+        <!-- Bannière -->
+        <div class="banniere_image">
+            <div id="banniere_description">
+                <p>TESTEZ VOS RÉFLEXES AVEC NOS DERNIERS TESTS PSYCHOTECHNIQUES</p>
+            </div>
+        </div>
+    </section>
+</body>
+    <!-- On ajoute le Footer -->
+    <?php require('Footer.html');
+    ?>
+</html>
