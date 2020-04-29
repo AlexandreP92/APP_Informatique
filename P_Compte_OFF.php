6b19f2968f9aa604c8a357da9dd4cc9b0a78c5fb
PAYEN <apayen@juniorisep.com>
2020-04-29 19:50:27 +0200

    retaffe page de connexion

---
+++ P_Compte_OFF.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link type="text/css" rel="stylesheet" href="P_Compte_OFF.css?t=<? echo time(); ?>" media="all">
+    <meta charset="UTF-8">
+    <title>WINK FOR INFINITE MEASURES</title>
+</head>
+<body>
+<!-- On ajoute le Header + Menu -->
+<?php require('Header.php');
+?>
+<?php require('Menu.html');
+?>
+
+<!-- Contenu de la Page Mon compte -->
+<section>
+    <p class="titreConnexion">Se connecter</p>
+    <form action="" method="post">
+        <label for="fname">NOM D'UTILISATEUR</label>
+        <input type="text" id="fname" name="utilisateur" placeholder="Votre nom d'utilisateur..">
+        <label for="fname">MOT DE PASSE</label>
+        <input type="password" id="fname" name="mdp" placeholder="*********">
+        <input type="submit" value="VALIDER">
+    </form>
+    <div id="boutonInscription">
+            <a class="inscription" href="P_Inscription.php">NOUVEL UTILISATEUR ? INSCRIS-TOI ! </a>
+    </div>
+
+
+
+
+</section>
+</body>
+<!-- On ajoute le Footer -->
+<?php require('Footer.html');
+?>
+</html>
