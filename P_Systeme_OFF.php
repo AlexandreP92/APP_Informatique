8c1531d9c27e434ff44adbc3595e244b3009886f
PAYEN <apayen@juniorisep.com>
2020-04-29 14:03:47 +0200

    V1 améliorée

---
+++ P_Systeme_OFF.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="P_Systeme_OFF.css" />
+    <title>WINK FOR INFINITE MEASURES</title>
+    <link rel="shortcut icon" href="favicon.ico" /><!-- Indispensable???? -->
+</head>
+
+<body>
+    <!-- On ajoute le Header + Menu -->
+    <?php require('Header.php');
+    ?>
+    <?php require('Menu.html');
+    ?>
+
+    <!-- Contenu de la Page Système -->
+    <section>
+        <h1>Les Tests :</h1>
+
+        <div id="conteneur">
+            <div class="element 1">
+                <p>Stimulus Sonore<br /></p>
+                <img src="../../../images/ear2.jpg" id="ear"  />
+                <p>Mesure le temps de réaction à <br />
+                    un son inattendu</p><br />
+            </div>
+
+            <div class="element 2">
+                <p>Stimulus Visuel<br /></p>
+                <img src="../../../images/eye2.png" id="eye"  />
+                <p> Mesure le temps de réaction à <br />
+                    une lumière inattendu</p><br />
+            </div>
+
+            <div class="element 3">
+                <p>Reconnaissance de tonalité<br /></p>
+                <img src="../../../images/sound2.jpg" id="sound"  />
+                <p> Mesure la capacité de l'utilisateur<br />
+                    à reproduire un son</p><br />
+            </div>
+
+        </div><br />
+
+        <img src="../../../images/arrow.png" id="arrow"  />
+
+        <div id="conteneur">
+            <div class="element 1">
+                <p>Mesure de la fréquence cardique<br /></p>
+                <img src="../../../images/heart2.png" id="ear"  />
+                <p>En positionant votre doigt nous mesurons votre  <br />
+                    frequence cardique à l'aide d'un capteur</p><br />
+            </div>
+
+            <div class="element 2">
+                <p>Mesure de la temperature corporelle<br /></p>
+                <img src="../../../images/thermo2.jpg" id="eye"  />
+                <p> Nous mesurons à l'aide d'un capteur adéquat, <br />
+                    votre température corporelle</p><br />
+            </div>
+
+        </div><br />
+    </section>
+</body>
+    <!-- On ajoute le Footer -->
+    <?php require('Footer.html');
+    ?>
+</html>
